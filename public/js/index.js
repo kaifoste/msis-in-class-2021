b27bdb76d8726b01039d5bfff699d2c0e1c735c5
kaifoste <fosterk202@gmail.com>
2021-09-15 09:54:19 -0400

    fetch

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 const Offer = {
     data() {
         return {
+            "result": {},
             "offers": [
                 {
                     "id": 1,
@@ -20,6 +21,22 @@ const Offer = {
                 }
             ]
         }
+    },
+    created() {
+        // creating http request, returing a promise immediately. Once its done i will do the then or the catch
+        fetch('https://randomuser.me/api')
+        // taking the fetch and putting it in variable response, response.json returns promise object
+        .then(response => response.json())
+        .then((json => {
+            // putting json in log so we can inspect it
+            console.log("Got json back:", json);
+            // updating data value. data binding 
+            this.result = json.results[0];
+            console.log("C";)
+        })
+        .catch((error) => {
+            console.error(error);
+        })
     }
 }
 
