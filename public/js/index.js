3e0329bc2014486426a077161045597ca894f4bc
kaifoste <fosterk202@gmail.com>
2021-09-20 11:43:04 -0400

    refresh

--- public/js/index.js
+++ public/js/index.js
@@ -1,43 +1,36 @@
-const Offer = {
+
+const SomeApp = {
     data() {
-        return {
-            "result": {},
-            "offers": [
-                {
-                    "id": 1,
-                    "name": "Jordan",
-                    "salary": 100000,
-                    "bonus": 10000,
-                    "company": "google",
-                    "offerDate": "2021-10-12"
-                },
-                {
-                    "id": 2,
-                    "name": "Doe",
-                    "salary": 200000,
-                    "bonus": 20000,
-                    "company": "microsoft",
-                    "offerDate": "2021-11-12"
-                }
-            ]
+      return {
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
+    },
+    computed: {
+        prettyBirthday() {
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
+        }
+    },
+    methods:{
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+        .then(response => response.json())
+        .then((json) => {
+            console.log("Got json back:", json);
+            this.result = json.results[0];
+            console.log("C");
+        })
+        .catch( (error) => {
+            console.error(error);
+        });
         }
     },
     created() {
-        // creating http request, returing a promise immediately. Once its done i will do the then or the catch
-        fetch('https://randomuser.me/api')
-        // taking the fetch and putting it in variable response, response.json returns promise object
-        .then(response => response.json())
-        .then((json => {
-            // putting json in log so we can inspect it
-            console.log("Got json back:", json);
-            // updating data value. data binding 
-            this.result = json.results[0];
-            console.log("C";)
-        })
-        .catch((error) => {
-            console.error(error);
-        })
+        this.fetchUserData();
     }
-}
 
-Vue.createApp(Offer).mount('#offerApp')
+  }
+  
+  Vue.createApp(SomeApp).mount('#someApp');
